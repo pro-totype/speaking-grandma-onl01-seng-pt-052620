39f39520c99d3283b8b0190439d9bb1c1e02e8f0
Sean B <butts.sean01@gmail.com>
2020-06-09 05:12:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma
-phrase= "I LOVE YOU GRANDMA"
+phrase= "I LOVE YOU GRANDMA!"
   
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
